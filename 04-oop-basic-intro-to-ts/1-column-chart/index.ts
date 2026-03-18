f463938eb42af33f42ac9a0e37705d050d516a63
Anton Ryabov <antonryabovdev@gmail.com>
2026-03-18 10:04:05 +0300

    solved 04-oop-basic-intro-to-ts

--- 04-oop-basic-intro-to-ts/1-column-chart/index.ts
+++ 04-oop-basic-intro-to-ts/1-column-chart/index.ts
@@ -1,11 +1,79 @@
-import { createElement } from "../../../shared/utils/create-element";
+import { createElement } from "../../shared/utils/create-element";
 
 interface Options {
-
+	data: number[];
+	label: string;
+	value: number;
+	link?: string;
+	formatHeading?: (data: number) => string;
 }
 
 export default class ColumnChart {
-  constructor({ }: Options = {}) {
-    
-  }
+	public element: HTMLElement;
+	private chartHeight: number = 50;
+
+	constructor(private props: Options = { data: [], label: "", value: 0 }) {
+		this.element = createElement(this.chartContent());
+	}
+
+	private chartContent() {
+		const { data, label, link, value, formatHeading } = this.props;
+
+		const isLoadingData = !data?.length ? "column-chart_loading" : "";
+		const hasLinkInChart = link
+			? `<a class="column-chart__link" href="${link}">View all</a>`
+			: "";
+		const chartValue = formatHeading ? formatHeading(value) : value;
+
+		const chartElements = this.dataHandler(data);
+
+		const content = `
+			<div class="column-chart ${isLoadingData}">
+				<div class="column-chart__title">${label}${hasLinkInChart}</div>
+				<div class="column-chart__container">
+					<div class="column-chart__header" data-element="header">${chartValue}</div>
+					<div class="column-chart__chart">
+						${chartElements}
+					</div>
+				</div>
+			</div>
+		`;
+
+		return content;
+	}
+
+	private dataHandler(data: number[]) {
+		if (!data || !data.length) return "";
+		const maxValue = Math.max(...data);
+		const scale = 50 / maxValue;
+
+		const chartElements = data
+			.map((chartValue) => {
+				const scaleChartValue = Math.floor(chartValue * scale);
+				const percentChartValue =
+					((chartValue / maxValue) * 100).toFixed(0) + "%";
+
+				return `<div style="--value: ${scaleChartValue}" data-tooltip="${percentChartValue}"></div>`;
+			})
+			.join("");
+
+		return chartElements;
+	}
+
+	private update(data: number[]) {
+		const chartElements = this.dataHandler(data);
+		const chart = document.querySelector(".column-chart__chart");
+
+		if (!chart) {
+			throw new Error("The chart is missing");
+		}
+		chart.innerHTML = "";
+		chart.insertAdjacentHTML("beforeend", chartElements);
+	}
+	private remove() {
+		this.element.remove();
+	}
+	private destroy() {
+		this.element.remove();
+	}
 }
